a9c8f2e80146228064738a9fad49d572c1a96ecd
AndreasC <andreascalvin21@gmail.com>
2023-09-27 00:51:46 +0700

    memperbaiki banner autoslide yang tidak berjalan

--- js/script.js
+++ js/script.js
@@ -17,14 +17,19 @@ function ValidationForm() {
 function showSlides() {
     let i;
     let slides = document.querySelectorAll(".slide");
-    
+
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
+
     slideIndex++;
     if (slideIndex > slides.length) {
         slideIndex = 1;
     }
-    slides[slideIndex - 1].style.display = "block";
+
+    if (slides[slideIndex - 1]) {
+        slides[slideIndex - 1].style.display = "block";
+    }
+
     setTimeout(showSlides, 2000);
 }
